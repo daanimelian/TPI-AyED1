bbf51a1f77a4da9f6d212b9fb6aa96034a8e04af
Federico Juan <99974552+FedeJuan@users.noreply.github.com>
2022-06-01 23:57:15 -0300

    Update ejercicios.cpp

--- ejercicios.cpp
+++ ejercicios.cpp
@@ -46,7 +46,7 @@ vector<gps> recorridoNoCubierto(viaje v, recorrido r, distancia u) {
     int i = 0; int k = 0; bool estaDentro = false;
     while (k < r.size()){
         while (i < v.size()){
-            if (distEnKM(obtenerPosicion(v[i]), r[k]) < (u/1000)) {
+            if (distEnKM(obtenerPosicion(v[i]), r[k]) < (u/1000)) { // el test case esta mal, va la u sola
                 estaDentro = true;
                 i = v.size();
             }
@@ -89,9 +89,9 @@ grilla construirGrilla(gps esq1, gps esq2, int n, int m) {
     while(k < n+1){
         while (j < m+1) {
             a = {(maxlatitud - ((k - 1) * ((maxlatitud - minlatitud) / n))),
-                 (minlongitud + ((j - 1)* ((maxlongitud - maxlongitud) / m)))};
+                 (minlongitud + ((j - 1)* ((maxlongitud - minlongitud) / m)))};
             b = {(maxlatitud - k * ((maxlatitud - minlatitud) / n)),
-                 (minlongitud + (j * ((maxlongitud - maxlongitud) / m)))};
+                 (minlongitud + (j * ((maxlongitud - minlongitud) / m)))};
             c = {a, b, {k, j}};
             resp.push_back(c);
             j = j + 1;
@@ -99,25 +99,23 @@ grilla construirGrilla(gps esq1, gps esq2, int n, int m) {
         k = k + 1;
         j = 1;
     }
-    return resp;
-
+    
     return resp;
 }
 
 /************************************* EJERCICIO cantidadDeSaltos ******************************/
 int cantidadDeSaltos(grilla g, viaje v) {
-    int resp = 0; int i = 0;
+int resp = 0; int i = 0;
     celda c; celda d;
     while (i < v.size()){
         c = buscoCeldaDeUnViaje(v[i], g);
         d = buscoCeldaDeUnViaje(buscoSiguientePunto(v,v[i]),g);
-        if(abs(filaDeNombre(nombreDeCelda(c)) - filaDeNombre(nombreDeCelda(d))) >= 2
-           || abs(columnaDeNombre(nombreDeCelda(c)) - columnaDeNombre(nombreDeCelda(d))) >= 2)
+        if (sqrt(pow((filaDeNombre(nombreDeCelda(c)) - filaDeNombre(nombreDeCelda(d))),2) +
+        pow((columnaDeNombre(nombreDeCelda(c))- columnaDeNombre(nombreDeCelda(d))),2)) > 1)
             resp = resp + 1;
         i = i + 1;
     }
     return resp;
-
 }
 
 

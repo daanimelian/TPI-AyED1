dda09b80fd07232345c77021643ef8b4047f4642
Federico Juan <99974552+FedeJuan@users.noreply.github.com>
2022-06-02 00:03:07 -0300

    Add files via upload

---
+++ testEnunciado/TestsPropios/construirGrillaTESTpropio.cpp
@@ -0,0 +1,23 @@
+#include "../ejercicios.h"
+#include "../auxiliares.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+
+/*TEST(cantidadDeSaltosTESTpropio, todosSaltos){
+gps p1 = {85,10};
+gps p2 = {85,50};
+gps p3 = {85,85};
+gps p4 = {50,10};
+gps p5 = {50,50};
+gps p6 = {50,85};
+gps p7 = {10,10};
+gps p8 = {10,50};
+gps p9 = {10,85};
+grilla g = construirGrilla({90,0},{0,90},4,4);
+viaje v1 = {{1,p1},{2,p9}};
+viaje v2 = {{5,p7},{1,p1},{2,p2},{4,p3},{6,p5},{3,p7},{7,p5}};
+EXPECT_EQ(cantidadDeSaltos(g,v1),1);
+EXPECT_EQ(cantidadDeSaltos(g,v2),3);
+}*/
